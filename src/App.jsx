b707d036e0cf9da3d8c9c014399edf718dec5023
SufyanAhmed1850 <sufyan.ahmed1850@gmail.com>
2023-12-30 14:34:25 +0500

    minor test changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,64 @@
-import React, { lazy, Suspense } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import Home from "./Pages/Home";
+import DetailedInvoice from "./Pages/DetailedInvoice";
+import CompanyDetails from "./Pages/CompanyDetails";
+import Login from "./Pages/login";
+import Signup from "./Pages/signup";
 import RequireAuth from "./Hooks/RequireAuth";
 import Auth from "./Hooks/Auth";
 import SidePanel from "./Components/SidePanel";
-
-// Lazy-loaded components
-const Home = lazy(() => import("./Pages/Home"));
-const DetailedInvoice = lazy(() => import("./Pages/DetailedInvoice"));
-const CompanyDetails = lazy(() => import("./Pages/CompanyDetails"));
-const Login = lazy(() => import("./Pages/login"));
-const Signup = lazy(() => import("./Pages/signup"));
 
 function App() {
     const location = useLocation();
     const isAuthRoute =
         location.pathname === "/login" || location.pathname === "/signup";
-
     return (
         <>
             {!isAuthRoute && <SidePanel />}
             <Toaster />
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <RequireAuth>
-                                <Home />
-                            </RequireAuth>
-                        }
-                    />
-                    <Route
-                        path="/invoice/:invoiceNumber"
-                        element={
-                            <RequireAuth>
-                                <DetailedInvoice />
-                            </RequireAuth>
-                        }
-                    />
-                    <Route
-                        path="/profile-details"
-                        element={
-                            <RequireAuth>
-                                <CompanyDetails />
-                            </RequireAuth>
-                        }
-                    />
-                    <Route
-                        path="/login"
-                        element={
-                            <Auth>
-                                <Login />
-                            </Auth>
-                        }
-                    />
-                    <Route
-                        path="/signup"
-                        element={
-                            <Auth>
-                                <Signup />
-                            </Auth>
-                        }
-                    />
-                </Routes>
-            </Suspense>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <RequireAuth>
+                            <Home />
+                        </RequireAuth>
+                    }
+                />
+                <Route
+                    path="/invoice/:invoiceNumber"
+                    element={
+                        <RequireAuth>
+                            <DetailedInvoice />
+                        </RequireAuth>
+                    }
+                />
+                <Route
+                    path="/profile-details"
+                    element={
+                        <RequireAuth>
+                            <CompanyDetails />
+                        </RequireAuth>
+                    }
+                />
+                <Route
+                    path="/login"
+                    element={
+                        <Auth>
+                            <Login />
+                        </Auth>
+                    }
+                />
+                <Route
+                    path="/signup"
+                    element={
+                        <Auth>
+                            <Signup />
+                        </Auth>
+                    }
+                />
+            </Routes>
         </>
     );
 }
